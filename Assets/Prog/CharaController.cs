8e4fe3a38be1ecff73b7bcdb285a2b6ae5231566
oceane <oceane.mettey@icloud.com>
2026-04-07 16:00:23 +0200

    Chara fin ?

--- Assets/Prog/CharaController.cs
+++ Assets/Prog/CharaController.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Windows;
 
 public class CharaController : MonoBehaviour
 {
@@ -10,166 +8,169 @@ public class CharaController : MonoBehaviour
     [SerializeField] private Rigidbody m_rb;
     [SerializeField] private Animator m_anim;
     [SerializeField] private GroundCheck m_groundCheck;
-    [SerializeField] private Transform m_spawnPoint;
 
-    [Header("Values")]
-    [SerializeField] private float m_acceleration = 10f;
-    [SerializeField] private float m_maxSpeed = 20f;
-    [SerializeField] private float m_dampingOnStop = 10f;
-    [SerializeField] private float m_jumpForce = 20;
-    [SerializeField] private float m_maxJumpDuration = 1;
-    [SerializeField] private float m_fallGravity = 2;
+    [Header("Movement")]
+    [SerializeField] private float m_acceleration = 30f;
+    [SerializeField] private float m_airControl = 0.5f;
+    [SerializeField] private float m_rotationSpeed = 15f;
+
+    [Header("Run")]
+    [SerializeField] private float m_walkSpeed = 3f;
+    [SerializeField] private float m_runSpeed = 6f;
+    [SerializeField] private float m_speedSmooth = 10f;
+
+    [Header("Jump")]
+    [SerializeField] private float m_jumpHeight = 2.5f;
+    [SerializeField] private float m_jumpTimeToApex = 0.4f;
+    [SerializeField] private float m_maxJumpHoldTime = 0.25f;
+    [SerializeField] private float m_fallMultiplier = 2.5f;
 
     [Header("Input")]
     [SerializeField] private InputActionReference m_moveInput;
+    [SerializeField] private InputActionReference m_runInput;
     [SerializeField] private InputActionReference m_jumpInput;
 
-    private Vector3 m_moveInputDirection;
-    private bool m_isMoving;
+    private Vector3 m_moveDirection;
     private bool m_isGrounded;
     private bool m_isJumping;
+    private float m_jumpTimer;
+    private float m_currentSpeed;
 
-    private float m_gravityMultiplier = 1;
+    private float m_gravity;
+    private float m_jumpVelocity;
+
     void Start()
     {
         m_moveInput.action.Enable();
+        m_runInput.action.Enable();
         m_jumpInput.action.Enable();
-    }
 
+        m_gravity = (-2f * m_jumpHeight) / Mathf.Pow(m_jumpTimeToApex, 2);
+        m_jumpVelocity = (2f * m_jumpHeight) / m_jumpTimeToApex;
+    }
 
     void Update()
     {
-        m_moveInputDirection = m_moveInput.action.ReadValue<Vector2>();
+        HandleInput();
+        UpdateSpeed();
+        HandleRotation();
+        HandleJumpInput();
+    }
 
-        m_moveInputDirection.z = m_moveInputDirection.y;
-        m_moveInputDirection.y = 0;
+    void FixedUpdate()
+    {
+        CheckGround();
+        HandleMovement();
+        ApplyGravity();
+    }
 
-        m_moveInputDirection = (Camera.main.transform.right * m_moveInputDirection.x) + (Camera.main.transform.forward * m_moveInputDirection.z);
-        m_moveInputDirection.y = 0;
-        m_moveInputDirection.Normalize();
+    private void HandleInput()
+    {
+        Vector2 input = m_moveInput.action.ReadValue<Vector2>();
 
-        if (m_moveInputDirection != Vector3.zero)
-            OnStartMoving();
-        else
-            OnStopMoving();
+        Vector3 camForward = Camera.main.transform.forward;
+        Vector3 camRight = Camera.main.transform.right;
 
-        SetOrientation();
+        camForward.y = 0;
+        camRight.y = 0;
 
-        if (m_jumpInput.action.WasPressedThisFrame() && CanJump())
+        m_moveDirection = (camForward.normalized * input.y + camRight.normalized * input.x).normalized;
+    }
+
+    private void HandleMovement()
+    {
+        float control = m_isGrounded ? 1f : m_airControl;
+
+        Vector3 targetVelocity = m_moveDirection * m_currentSpeed;
+        Vector3 velocity = m_rb.linearVelocity;
+
+        Vector3 velocityChange = (targetVelocity - new Vector3(velocity.x, 0, velocity.z)) * m_acceleration * control;
+
+        m_rb.AddForce(velocityChange, ForceMode.Acceleration);
+    }
+
+    private void HandleRotation()
+    {
+        if (m_moveDirection.sqrMagnitude < 0.01f)
+            return;
+
+        Vector3 targetForward = Vector3.RotateTowards(
+            transform.forward,
+            m_moveDirection,
+            m_rotationSpeed * Time.deltaTime,
+            0f
+        );
+
+        transform.forward = targetForward;
+    }
+
+    private void UpdateSpeed()
+    {
+        bool isRunning = m_runInput.action.IsPressed();
+
+        float targetSpeed = isRunning ? m_runSpeed : m_walkSpeed;
+
+        m_currentSpeed = Mathf.Lerp(m_currentSpeed, targetSpeed, Time.deltaTime * m_speedSmooth);
+
+        //m_anim.SetFloat("Run", m_rb.linearVelocity.magnitude);
+    }
+
+    private void HandleJumpInput()
+    {
+        if (m_jumpInput.action.WasPressedThisFrame() && m_isGrounded)
         {
-            OnJump();
+            Jump();
+        }
+
+        if (m_jumpInput.action.IsPressed() && m_isJumping)
+        {
+            m_jumpTimer += Time.deltaTime;
+
+            if (m_jumpTimer < m_maxJumpHoldTime)
+            {
+                m_rb.AddForce(Vector3.up * m_jumpVelocity * 0.5f, ForceMode.Acceleration);
+            }
+        }
+
+        if (m_jumpInput.action.WasReleasedThisFrame())
+        {
+            m_isJumping = false;
         }
     }
 
-    private void FixedUpdate()
+    private void Jump()
     {
-        SetDamping();
+        Vector3 velocity = m_rb.linearVelocity;
+        velocity.y = 0;
+        m_rb.linearVelocity = velocity;
 
-        SetGravityMultiplier();
+        m_rb.AddForce(Vector3.up * m_jumpVelocity, ForceMode.VelocityChange);
 
-        SetIsGround(m_groundCheck.GetIsHitting());
-
-        m_rb.AddForce((Vector3)m_moveInputDirection * m_acceleration, ForceMode.Acceleration);
-
-        m_rb.AddForce(Physics.gravity * (m_gravityMultiplier - 1), ForceMode.Acceleration);
-
-        float directionDot = Vector3.Dot(m_rb.linearVelocity.normalized, m_moveInputDirection.normalized);
-        if (directionDot > 0.8f)
-            m_rb.linearVelocity *= 0.8f;
-
-        float yVelo = m_rb.linearVelocity.y;
-        m_rb.linearVelocity = Vector3.ClampMagnitude(m_rb.linearVelocity, m_maxSpeed);
-        m_rb.linearVelocity = new Vector3(m_rb.linearVelocity.x, yVelo, m_rb.linearVelocity.z);
-    }
-
-    private void OnStartMoving()
-    {
-        if (!m_isMoving)
-        {
-            m_isMoving = true;
-
-            m_anim.SetTrigger("OnStartMoving");
-        }
-    }
-
-    private void OnStopMoving()
-    {
-        if (!m_isMoving)
-        {
-            m_isMoving = false;
-
-            m_anim.SetTrigger("OnStopMoving");
-        }
-    }
-
-    private void SetDamping()
-    {
-        if (!m_isGrounded)
-            if (m_isMoving) m_rb.linearDamping = 0;
-            else m_rb.linearDamping = m_dampingOnStop;
-
-        else
-            m_rb.linearDamping = 1;
-    }
-
-    private void SetOrientation()
-    {
-        transform.forward = Vector3.RotateTowards(transform.forward, m_moveInputDirection, Time.deltaTime * 20, 0);
-    }
-
-    private void OnJump()
-    {
-        m_rb.linearDamping = 1;
-
-        m_rb.AddForce(Vector3.up * m_jumpForce, ForceMode.Impulse);
         m_isJumping = true;
-        
-        StartCoroutine(C_Jump());
+        m_jumpTimer = 0;
+
+        //if (m_anim) m_anim.SetTrigger("Jump");
     }
 
-    private void OnJumpEnd()
+    private void ApplyGravity()
     {
-        m_isJumping = false;
-    }
-
-    private IEnumerator C_Jump()
-    {
-        float timer = 0;
-        float duration = m_maxJumpDuration;
-
-        while (timer < duration)
+        if (m_rb.linearVelocity.y < 0)
         {
-            timer += Time.fixedDeltaTime;
-            yield return new WaitForFixedUpdate();
+            m_rb.AddForce(Vector3.up * m_gravity * m_fallMultiplier, ForceMode.Acceleration);
         }
-
-        OnJumpEnd();
-    }
-
-    private void SetIsGround (bool value)
-    {
-        m_isGrounded = value;
-    }
-
-    private bool CanJump()
-    {
-        if (!m_isGrounded) return false;
-
-        return true;
-    }
-
-    private void SetGravityMultiplier()
-    {
-        if (m_isGrounded)
-            m_gravityMultiplier = 1;
-
         else
         {
-            if (m_isJumping)
-                m_gravityMultiplier = 1;
-            else
-                m_gravityMultiplier = m_fallGravity;
+            m_rb.AddForce(Vector3.up * m_gravity, ForceMode.Acceleration);
+        }
+    }
 
+    private void CheckGround()
+    {
+        m_isGrounded = m_groundCheck.GetIsHitting();
+
+        if (m_isGrounded)
+        {
+            m_isJumping = false;
         }
     }
 }
